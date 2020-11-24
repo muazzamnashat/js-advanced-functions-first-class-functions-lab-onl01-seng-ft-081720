c86457cb2453f7cca4088f547c40994826ccf181
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-11-24 18:11:43 -0500

    commit tree 3e41cf64d03e3d680de459f257f959fbb99ca4f8

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (array) =>{
+    return array.slice(0,2)
+}
+
+const returnLastTwoDrivers = (array) =>{
+    return array.slice(-2)
+}
+
+const selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers]
+
+const createFareMultiplier = (int) => {
+    return function (fare) {
+        return fare * int
+    }
+}
+
+const fareDoubler = (fare) =>{
+    return createFareMultiplier(2)(fare)
+}
+
+const fareTripler = (fare) =>{
+    return createFareMultiplier(3)(fare)
+}
+
+const selectDifferentDrivers = (array,func) =>{
+    return func(array)
+}
